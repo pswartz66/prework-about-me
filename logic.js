8864a60318ef15cba641261b760714df6a90a434
Phil Swartz <pswartz.sju@gmail.com>
2019-07-29 21:02:34 -0400

    modified index and logic

--- logic.js
+++ logic.js
@@ -3,3 +3,11 @@ var t = setInterval(function () {
     var ele = document.getElementById('myBlink');
     ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden');
 }, blink_speed);
+
+
+function growText() {
+    document.getElementById("myHead").addEventListener("mouseover", function(){
+        document.getElementsByTagName("section").style.height = "400px";
+
+});
+};
